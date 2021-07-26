0cd3b16f5e0c6147d9e0d0bc3edfbea96bbe3443
Marshall <363752922@qq.com>
2021-07-26 15:00:38 -0400

    Add project files.

---
+++ Haocheng_Zhao.ClientInfoSystem.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Haocheng_Zhao.ClientInfoSystem.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
